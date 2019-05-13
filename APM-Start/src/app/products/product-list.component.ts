32a60c3ce7b33987463a6edd69891db248a4de69
Irina O'Hara <iohara@rmoadmins-MacBook-Pro.local>
2019-05-13 17:00:34 -0600

    add directives

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+    pageTitle: string = 'Product List!';
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/svg/"
+        },
+        {
+            "productId": 3,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/svg/"
+        }
+    ];
+}
