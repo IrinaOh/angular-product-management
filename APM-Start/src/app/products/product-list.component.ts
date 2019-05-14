d3b7bf398985971b64a58bb492d9bcdead79c5d0
Irina O'Hara <iohara@rmoadmins-MacBook-Pro.local>
2019-05-14 12:24:04 -0600

    add pipes, interface, format code and price

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List!';
-    products: any[] = [
+    imageWidth: number = 50;
+    imageMargin: 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -15,27 +21,34 @@ export class ProductListComponent {
             "description": "15 gallon capacity rolling garden",
             "price": 32.99,
             "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg/"
+            "imageUrl": "http://clipart-library.com/images/8ixrxEXAT.jpg"
         },
         {
             "productId": 3,
-            "productName": "Garden Cart",
+            "productName": "Garden Scissors",
             "productCode": "GDN-0023",
             "releaseDate": "March 18, 2016",
             "description": "15 gallon capacity rolling garden",
-            "price": 32.99,
+            "price": 2.99,
             "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg/"
+            "imageUrl": "http://clipart-library.com/img1/371666.png"
         },
         {
             "productId": 4,
-            "productName": "Garden Cart",
+            "productName": "Garden Shovel",
             "productCode": "GDN-0023",
             "releaseDate": "March 18, 2016",
             "description": "15 gallon capacity rolling garden",
-            "price": 32.99,
+            "price": 5.95,
             "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg/"
+            "imageUrl": "http://clipart-library.com/img1/1445505.png"
         }
     ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
 }
